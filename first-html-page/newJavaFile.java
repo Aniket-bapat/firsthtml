a34cf581356d3289fe8b30e3e45f1b236a944a0d
Aniket-bapat <135721728+Aniket-bapat@users.noreply.github.com>
2024-05-08 10:31:45 +0530

    Add files via upload

---
+++ first-html-page/newJavaFile.java
@@ -0,0 +1,13 @@
+class ex1 {
+   public  void display(){
+        
+        System.out.println("HELLO");
+        
+    }
+}
+class a{
+    public static void main(String args[]){
+        ex1 A=new ex1();
+        A.display();
+    }
+}
